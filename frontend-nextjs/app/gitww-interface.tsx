070980b31b672986c77486c0c440be0284555432
0xMichaelRan <michaelran16@gmail.com>
2024-11-20 22:06:18 +0800

    settings -> config

--- frontend-nextjs/app/gitww-interface.tsx
+++ frontend-nextjs/app/gitww-interface.tsx
@@ -160,10 +160,10 @@ export default function GitWW() {
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                   <SidebarMenuItem>
-                    <Link href="/settings" passHref>
+                    <Link href="/config" passHref>
                       <SidebarMenuButton>
                         <Settings className="mr-2 h-4 w-4" />
-                        Settings
+                        Config
                       </SidebarMenuButton>
                     </Link>
                   </SidebarMenuItem>
