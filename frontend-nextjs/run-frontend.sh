4a9aaf135198796daea6fe7f9d72dcc0c489608e
0xMichaelRan <michaelran16@gmail.com>
2024-11-22 12:22:32 +0800

    add a master script to run 2 other scripts

--- frontend-nextjs/run-frontend.sh
+++ frontend-nextjs/run-frontend.sh
@@ -1,5 +1,3 @@
 echo -ne "\033]0;gitww-frontend\007"
 cd ~/hub/gitww-monorepo/frontend-nextjs
-cursor ..
-open "http://localhost:3000/"
 npm run dev
---
+++ run-gitww.sh
@@ -0,0 +1,4 @@
+cursor ~/hub/gitww-monorepo/ \
+& open -a iTerm ~/hub/gitww-monorepo/frontend-nextjs/run-frontend.sh \
+& open -a iTerm ~/hub/gitww-monorepo/backend-fastapi/run-backend.sh \
+& open "http://localhost:3000/"
